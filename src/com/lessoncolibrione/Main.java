a293958f748a347bc6aafda447c6b3bd96bd3aec
WhoesQuestionMyAbility <34679828+WhoesQuestionMyAbility@users.noreply.github.com>
2018-01-30 22:34:44 +0700

    Added lesson 1 (4) content

---
+++ src/com/lessoncolibrione/Main.java
@@ -0,0 +1,12 @@
+package com.lessoncolibrione;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.print("Hello World");
+        n();
+    }
+
+    static void n() {
+        System.out.print("NOT work!");
+    }
+}
