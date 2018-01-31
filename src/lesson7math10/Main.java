663b0188782b9a84a923b33a5d07f7568c539b8f
WhoesQuestionMyAbility <34679828+WhoesQuestionMyAbility@users.noreply.github.com>
2018-01-31 12:59:23 +0700

    Added ls 7(10) math

---
+++ src/lesson7math10/Main.java
@@ -0,0 +1,21 @@
+package lesson7math10;
+
+public class Main {
+    public static void main(String[] args){
+
+        int a = 10;
+        double b = 5.55;
+        int c = -4;
+
+
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(Math.abs(c)); //Модуль числа c
+        System.out.println(Math.sin(b)); //Синус b
+        System.out.println(Math.PI); //Вывод числа пи
+        System.out.println(Math.cos(b) + c);
+    }
+}
