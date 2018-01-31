97182e6bd14caaa7b7646ea4aa2990fbc27f1094
WhoesQuestionMyAbility <34679828+WhoesQuestionMyAbility@users.noreply.github.com>
2018-01-31 18:06:41 +0700

    added lesson14(17) Naming Convention

---
+++ src/lesson14_17namingconvention/JavaExamplesCode.java
@@ -0,0 +1,14 @@
+package lesson14_17namingconvention;
+
+public class JavaExamplesCode { //Название классов с большой буквы в CamelCase
+    int ageDog; //Переменные = существительное
+    int ageCat; //Начинается с маленькой буквы в camelCase
+
+    void getEmail(){
+        //Методы = глагол
+    }
+
+    void sendThisEmailFromSecondEmail(){
+        //Начинается с маленькой буквы в camelCase
+    }
+}
